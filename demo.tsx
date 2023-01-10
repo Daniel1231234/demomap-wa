e3228fa4bf057129315caf73b9413009a0ed13ab
Daniel Shechter <dshctr1993@gmail.com>
2023-01-10 18:00:50 +0200

    videos+another map added

---
+++ demo.tsx
@@ -0,0 +1,31 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="demo" tilewidth="2760" tileheight="3655" tilecount="9" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="1000" height="460" source="tilesets/baby.png"/>
+ </tile>
+ <tile id="1">
+  <image width="500" height="484" source="tilesets/cd.png"/>
+ </tile>
+ <tile id="2">
+  <image width="1024" height="576" source="tilesets/fortis.png"/>
+ </tile>
+ <tile id="3">
+  <image width="2760" height="3655" source="tilesets/guitar.png"/>
+ </tile>
+ <tile id="4">
+  <image width="236" height="214" source="tilesets/oldTV.png"/>
+ </tile>
+ <tile id="5">
+  <image width="1920" height="1080" source="tilesets/sendwatch.png"/>
+ </tile>
+ <tile id="6">
+  <image width="720" height="480" source="tilesets/space1.png"/>
+ </tile>
+ <tile id="7">
+  <image width="700" height="646" source="tilesets/space2.png"/>
+ </tile>
+ <tile id="8">
+  <image width="612" height="378" source="tilesets/stage.png"/>
+ </tile>
+</tileset>
