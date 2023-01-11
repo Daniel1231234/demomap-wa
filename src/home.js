fb0064dc6f53e9882fee63158b6c88dfba9c8a65
Daniel Shechter <dshctr1993@gmail.com>
2023-01-11 14:44:09 +0200

    concert video has  added

---
+++ src/home.js
@@ -0,0 +1,24 @@
+window.onload = function () {
+  const host = window.location.host
+  let path = window.location.pathname
+  if (path.endsWith("index.html")) {
+    path = path.substr(path, path.length - "index.html".length)
+  }
+  const instanceId = Math.random().toString(36).substring(2, 15)
+  const url = `https://play.workadventu.re/_/${instanceId}/${host}${path}maps/demomap.json`
+  document.getElementById("testMapURL").href = url
+  document.getElementById("testMapBtnURL").href = url
+  document.getElementById("testMapURL").innerText = url
+
+  const jsonURL =
+    window.location.protocol +
+    "//" +
+    window.location.host +
+    path +
+    "maps/demomap.json"
+  // document.getElementById('jsonURL').innerHTML = jsonURL;
+
+  // const gettingStartedLink =
+  //   "https://workadventu.re/getting-started?name=Map&mapUrl=" + jsonURL
+  // document.getElementById("gettingStartedLink").href = gettingStartedLink
+}
